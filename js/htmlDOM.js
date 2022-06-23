e3759474fcf8f9475dc7c1183e98c41ab2cb6cb1
JulyAnAr88 <july.an.ar88@gmail.com>
2022-06-22 21:11:41 -0300

    con grid

--- js/htmlDOM.js
+++ js/htmlDOM.js
@@ -30,3 +30,21 @@ function createElementHtml(request, id) {
     }
     return response;
 }
+
+function pageChandler(request) {
+    var request = document.querySelector(request);
+    for (var i = 0; i < 3; i++) {
+        var search = ".page_" + (i + 1);
+        var main = document.querySelector(search);
+        visibilityMain(main, false);
+    }
+    visibilityMain(request, true);
+}
+
+function visibilityMain(page, action) {
+    if (action == true) {
+        page.style.display = "block";
+    } else {
+        page.style.display = "none";
+    }
+}
